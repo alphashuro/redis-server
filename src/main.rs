b3e46d72cf89cbf412c9b1c2afcbf884aff15bb4
Alpha Shuro <alphashuro@gmail.com>
2023-01-26 00:19:30 +0100

    ping

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, BufWriter, Read, Write},
+    net::TcpListener,
+};
+
+enum Command {
+    PING,
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,8 +15,32 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+
+                let reader = BufReader::new(&stream);
+                let mut writer = BufWriter::new(&stream);
+
+                for msg in reader.lines() {
+                    match msg {
+                        Ok(str) => {
+                            let response = match str.to_lowercase().as_str() {
+                                "+ping" => "+PONG",
+                                _ => "+PONG",
+                            };
+
+                            println!("msg: {}\nres: {}\n", str, response);
+
+                            writer
+                                .write_all(format!("{}\r\n", response).as_bytes())
+                                .expect("Thought I could write back!?");
+                            writer.flush().expect("Couldn't flush");
+                        }
+                        Err(e) => {
+                            println!("error receiving: {}", e);
+                        }
+                    }
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
