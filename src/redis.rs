873a06f89ef66382274fe9b3fec64776ba2637c0
Alpha Shuro <alphashuro@gmail.com>
2023-04-03 18:34:48 +0200

    add expiration

--- src/redis.rs
+++ src/redis.rs
@@ -1,7 +1,16 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    time::{Duration, Instant},
+};
 
 pub struct Server {
-    store: HashMap<String, String>,
+    store: HashMap<String, Data>,
+}
+
+struct Data {
+    value: String,
+    inserted_at: Instant,
+    expires_in: Option<Duration>,
 }
 
 impl Server {
@@ -19,12 +28,28 @@ impl Server {
             Ping => SimpleString("PONG".to_string()),
             Command(SubCommand::Docs) => SimpleString("OK".to_string()),
             Echo(text) => SimpleString(text.to_string()),
-            Set(key, value) => {
-                self.store.insert(key.to_string(), value.to_string());
+            Set(key, value, options) => {
+                let data = Data {
+                    value: value.to_string(),
+                    inserted_at: Instant::now(),
+                    expires_in: options.px.map(|exp| Duration::from_millis(exp)),
+                };
+                self.store.insert(key.to_string(), data);
+
                 SimpleString("OK".to_string())
             }
             Get(key) => match self.store.get(key) {
-                Some(value) => BulkString(value.to_string()),
+                Some(data) => {
+                    if let Some(duration) = data.expires_in {
+                        if Instant::now() > (data.inserted_at + duration) {
+                            self.store.remove(key);
+
+                            return BulkString("nil".to_string());
+                        }
+                    }
+
+                    BulkString(data.value.to_string())
+                }
                 None => BulkString("nil".to_string()),
             },
             _ => todo!(),
@@ -52,8 +77,23 @@ pub enum RedisCmd {
     Ping,
     Echo(String),
     Command(SubCommand),
-    Set(String, String),
+    Set(String, String, SetOptions),
     Get(String),
+}
+
+#[derive(Debug)]
+pub struct SetOptions {
+    px: Option<u64>,
+}
+
+impl SetOptions {
+    fn new() -> Self {
+        SetOptions { px: None }
+    }
+
+    fn set_px(&mut self, value: u64) {
+        self.px = Some(value);
+    }
 }
 
 pub fn parse_command(strings: &Vec<String>) -> Result<RedisCmd, String> {
@@ -66,7 +106,24 @@ pub fn parse_command(strings: &Vec<String>) -> Result<RedisCmd, String> {
         ["ping"] => Ok(Ping),
         ["command", "docs"] => Ok(Command(SubCommand::Docs)),
         ["echo", text] => Ok(Echo(text.to_string())),
-        ["set", key, value] => Ok(Set(key.to_string(), value.to_string())),
+        ["set", key, value, rest @ ..] => {
+            let mut set_options = SetOptions::new();
+
+            let mut iter = rest.iter();
+            loop {
+                let option = iter.next();
+                match option {
+                    Some(&"px") => {
+                        let value = iter.next().unwrap().parse().unwrap();
+                        set_options.set_px(value);
+                    }
+                    Some(_) => continue,
+                    None => break,
+                }
+            }
+
+            Ok(Set(key.to_string(), value.to_string(), set_options))
+        }
         ["get", key] => Ok(Get(key.to_string())),
         _ => Err(format!("command not known: {:?}", strings)),
     }
